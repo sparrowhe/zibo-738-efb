199b088bfbaf87faa262951fdce92b3a3c94066b
Eureka0225 <1441373096@qq.com>
2020-03-17 20:38:53 +0800

    完成已框选项目的保存和再次读取（？）

--- js/efb.js
+++ js/efb.js
@@ -1,11 +1,12 @@
 function show(n) {
-    var name = ["beforeFlight.html", "beforeStart.html", "beforeTaxi.html", "beforeTakeoff.html",
-        "afterTakeoff.html", "descend.html", "approach.html", "landing.html", "cutoff.html", "leave.html"
+    var name = ["beforeFlight", "beforeStart", "beforeTaxi", "beforeTakeoff",
+        "afterTakeoff", "descend", "approach", "landing", "cutoff", "leave"
     ];
     $.ajax({
-        url: "checklist/" + name[n - 1],
+        url: "checklist/" + name[n - 1] + ".html",
         success: function (result) {
             $("#card").html(result);
+            read(name[n - 1]);
         }
     })
 }
@@ -24,11 +25,12 @@ function qrh() {
     $("#foot").removeClass("remove");
     show(1);
 }
+
 function upd(n) {
     url = "https://wiki.sinofsx.com/Charts/ENR/ENR_ERC" + n + ".pdf";
     targeturl = "pdfjs/web/viewer.html?file=" + url;
-    html=$("#2").html();
-    $("#2").html('<object style="height:100%;width:100%;" data="'+targeturl+'">' +'</object>' + html);
+    html = $("#2").html();
+    $("#2").html('<object style="height:100%;width:100%;" data="' + targeturl + '">' + '</object>' + html);
     $("#1").addClass("remove");
     $("#2").removeClass("remove");
 }
